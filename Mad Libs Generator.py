9d473b817d196853eadd6d9cd8d7813de19ea8a4
shafiq <shafiq_razman@yahoo.com>
2020-07-31 17:02:47 +0800

    Initial commit

---
+++ Mad Libs Generator.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    color = input("Fav color: ")
+    hobby = input("Hobby: ")
+    age = input("Age: ")
+    print(f"My fav color is {color}")
+    print(f"My hobby is {hobby}")
+    print(f"My age is {age}")
